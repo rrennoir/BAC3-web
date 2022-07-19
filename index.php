e71fa33ef2ae67e8c418fbdfe24b217fc1317456
Ryan Rennoir <32205591+rrennoir@users.noreply.github.com>
2022-07-19 09:09:25 +0200

    html does't support php by default

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
         Today’s date is <b>
             <?php echo date('Y/m/d') ?>
         </b> and it’s a <b>
-            <?php echo date(‘l’) ?>
+            <?php echo date('l') ?>
         </b> today!
     </div>
 
