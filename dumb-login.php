0155aeb1848300805aa476831dc50b6ed4a3d4fe
Ryan Rennoir <32205591+rrennoir@users.noreply.github.com>
2022-07-19 14:16:12 +0200

    mmhh

--- dumb-login.php
+++ dumb-login.php
@@ -14,19 +14,20 @@
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    $result = $conn->query("SELECT * FROM users WHERE $username = username AND $password = password;");
+    $result = $conn->query("SELECT * FROM users WHERE username = $username AND password = $password;");
 
     if (!$result) {
         echo "Querry error: " . $conn->error . "<br>";
     }
-
-    if ($result->num_row > 0) {
-        echo "Welcome $username" . "<br>";
-    } else {
-        if (!$conn->query("INSERT INTO users ('username', 'password') VALUES ('$username', '$password');")) {
-            echo "Querry error: " . $conn->error . "<br>";
+    else{
+        if ($result->num_row > 0) {
+            echo "Welcome $username" . "<br>";
+        } else {
+            if (!$conn->query("INSERT INTO users ('username', 'password') VALUES ('$username', '$password');")) {
+                echo "Querry error: " . $conn->error . "<br>";
+            }
+            echo "User $username with password $password added to DB" . "<br>";
         }
-        echo "User $username with password $password added to DB" . "<br>";
     }
 
     ?>
