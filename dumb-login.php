647a9dc6de80311adb089e82c67770ddcd704740
Ryan Rennoir <32205591+rrennoir@users.noreply.github.com>
2022-07-19 14:13:39 +0200

    say hello for existing user

--- dumb-login.php
+++ dumb-login.php
@@ -16,15 +16,17 @@
 
     $result = $conn->query("SELECT * FROM users WHERE $username = username AND $password = password;");
 
+    if (!$result) {
+        echo "Querry error: " . $conn->error . "<br>";
+    }
+
     if ($result->num_row > 0) {
-        while ($row = $result->fetch_assoc()) {
-            echo "id: " . $row["id"] . " - Username: " . $row["username"] . " - Password: " . $row["password"] . " " . "<br>";
-        }
+        echo "Welcome $username" . "<br>";
     } else {
         if (!$conn->query("INSERT INTO users ('username', 'password') VALUES ('$username', '$password');")) {
-            echo "Querry error: " . $conn->error;
+            echo "Querry error: " . $conn->error . "<br>";
         }
-        echo "User $username with password $password added to DB";
+        echo "User $username with password $password added to DB" . "<br>";
     }
 
     ?>
