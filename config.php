1682f7a14d0a5c31b0ac7bd1d2943c09244df581
Ryan Rennoir <32205591+rrennoir@users.noreply.github.com>
2022-07-19 09:12:52 +0200

    switch to php syntax

--- config.php
+++ config.php
@@ -7,10 +7,10 @@ define('DB_PASSWORD', 'very_secure');
 define('DB_NAME', 'web');
  
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 // Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+if($link->connect_error){
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
