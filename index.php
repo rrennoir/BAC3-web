4a9835ea7bcc4cc9e4741e18166bd52109e19a80
Ryan Rennoir <32205591+rrennoir@users.noreply.github.com>
2022-07-28 11:20:09 +0200

    Add link to my exams page

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ session_start();
                     </button>
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="/profile.php">Profile</a></li>
-                        <li><a class="dropdown-item" href="#">My exams</a></li>
+                        <li><a class="dropdown-item" href="/StudentExam.php">My exams</a></li>
                         <li><a class="dropdown-item" href="/logout.php">Logout</a></li>
                     </ul>
                 </div>
